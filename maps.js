79cf2eb829008f115d2fcd43322d835f005270a5
juanda <juandacorreo@gmail.com>
2022-11-25 09:36:50 +0100

    first commit

---
+++ maps.js
@@ -0,0 +1,28 @@
+import { Loader } from '@googlemaps/js-api-loader'
+
+const mapOptions = {
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+  zoom: 4,
+}
+
+export const cargarMapa = () => {
+  console.log('configurando librería....')
+  const loader = new Loader({
+    apiKey: '',
+    version: 'weekly',
+    libraries: ['places'],
+  })
+
+  // Promise
+  loader
+    .load()
+    .then((google) => {
+      new google.maps.Map(document.querySelector('#map'), mapOptions)
+    })
+    .catch((e) => {
+      // do something
+    })
+}
